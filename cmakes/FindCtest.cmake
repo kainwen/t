b245dde3a449b9e8fe9150f246988d36d5391551
kainwen <kainwen@gmail.com>
2018-03-07 20:46:32 +0800

    improve cmake

--- cmakes/FindCtest.cmake
+++ cmakes/FindCtest.cmake
@@ -2,4 +2,6 @@ find_path(CTEST_INCLUDE_DIR ctest.h
   PATHS /usr/include /usr/local/include
   DOC "Path to the ctest headers")
 
+message(STATUS "Found Ctest include path: ${CTEST_INCLUDE_DIR}")
+
 set(CTEST_INCLUDE_DIRS ${CTEST_INCLUDE_DIR})
